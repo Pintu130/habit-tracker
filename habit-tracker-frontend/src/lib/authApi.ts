74d9a96dc8dba794ddd864553f6cda50d098f647
Pintu130 <pin2sharma907@gmail.com>
2025-06-28 19:47:28 +0530

    add env file and deploy backend on render

--- habit-tracker-frontend/src/lib/authApi.ts
+++ habit-tracker-frontend/src/lib/authApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/",
+    baseUrl: import.meta.env.VITE_BASE_URL,
   }),
   endpoints: (builder) => ({
     login: builder.mutation({
@@ -46,5 +46,5 @@ export const {
   useRegisterMutation,
   useGetUsersQuery,
   useForgotPasswordMutation,
-  useResetPasswordMutation
+  useResetPasswordMutation,
 } = authApi;
